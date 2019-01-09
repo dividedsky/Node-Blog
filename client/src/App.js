c44e00c4067a21e276ee558f537bcb17cdb6d193
Justin <jdlow2@gmail.com>
2019-01-09 18:21:49 -0500

    Adds styles for user list

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Users from './components/Users';
 
@@ -7,7 +6,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>client app</h1>
         <Users />
       </div>
     );
--- client/src/components/Users.js
+++ client/src/components/Users.js
@@ -1,9 +1,46 @@
 import React from 'react';
 import axios from 'axios';
+import styled, {ThemeProvider} from 'styled-components';
 
 const ax = axios.create({
-  baseUrl: 'http://localhost:5000', // this isn't working for some reason
+  baseURL: 'http://localhost:5000', // this isn't working for some reason
 });
+
+const theme = {
+  granite: '#6d6466',
+  gray: '#9f9f92',
+  lightGreen: '#d0fcb3',
+  darkGreen: '#9bc59d',
+  black: '#271f30',
+};
+
+const UserList = styled.div`
+  width: 60%;
+  border: 2px solid ${props => props.theme.black};
+  background-color: ${props => props.theme.darkGreen};
+  margin: 0 auto;
+  box-shadow: 4px 4px 10px ${props => props.theme.black};
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
+
+const User = styled.div`
+  border: 2px solid ${props => props.theme.black};
+  width: 50%;
+  padding: 10px;
+  margin: 10px;
+  box-shadow: 2px 2px 10px ${props => props.theme.granite};
+  color: ${props => props.theme.granite};
+  transition: all 0.2s ease-out;
+
+  &:hover {
+    color: ${props => props.theme.black};
+    font-weight: bold;
+    transform: scale(1.2, 1.5);
+    background-color: ${props => props.theme.gray};
+  }
+`;
 
 class Users extends React.Component {
   constructor() {
@@ -14,7 +51,7 @@ class Users extends React.Component {
   }
 
   componentDidMount() {
-    ax.get('http://localhost:5000/users')
+    ax.get('/users')
       .then(res => {
         this.setState({users: res.data});
       })
@@ -26,12 +63,18 @@ class Users extends React.Component {
   render() {
     if (!this.state.users.length) return <h3>loading</h3>;
     return (
-      <div>
-        <h2>users</h2>
-        {this.state.users.map(user => (
-          <p>{user.name}</p>
-        ))}
-      </div>
+      <ThemeProvider theme={theme}>
+        <>
+          <h2>users</h2>
+          <UserList>
+            {this.state.users.map(user => (
+              <User key={user.id}>
+                <p>{user.name}</p>
+              </User>
+            ))}
+          </UserList>
+        </>
+      </ThemeProvider>
     );
   }
 }
