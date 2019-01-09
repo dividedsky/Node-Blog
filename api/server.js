22813741270cc38f48d9b4ae0601fc414cfef8ab
Justin <jdlow2@gmail.com>
2019-01-09 15:28:29 -0500

    Implements get to /users to fetch list of users

--- api/server.js
+++ api/server.js
@@ -1,3 +1,6 @@
+const userDb = require('../data/helpers/userDb');
+const postDb = require('../data/helpers/postDb');
+
 const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
@@ -12,6 +15,18 @@ server.use(helmet());
 // routes
 server.get('/', (req, res) => {
   res.status(200).send('sanity check!');
+});
+
+// user routes
+server.get('/users/', (req, res) => {
+  userDb
+    .get()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => {
+      errorMessage: `there was an error retrieving the users: ${err}`;
+    });
 });
 
 // export server
